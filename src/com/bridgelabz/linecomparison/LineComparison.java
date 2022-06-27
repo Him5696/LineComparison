13428ddbfde9f70e99ec1f65f153e1de8e8e78a5
Him5696 <himanshi1252@gmail.com>
2022-06-27 15:56:23 +0530

    Comparing Two Lengths of Lines using oops

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,45 +3,39 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
+    static int x1,y1,x2,y2;
     public static void main(String[] args) {
-        System.out.println("Welcome to lin Comparison computation Problem");
-        System.out.println("Enter the first (x1,y1) point");
-        int x1 = getInputFromUser("x1");
-        int y1 = getInputFromUser("y1");
-        System.out.println("Enter the second (x2, y2) point");
-        int x2 = getInputFromUser("x2");
-        int y2 = getInputFromUser("y2");
-        double x = Math.pow((x2 - x1), 2);
-        double y = Math.pow((y2 - y1), 2);
-        Double length = (Math.sqrt(x + y));
-        System.out.println("Length of the line : "+length);
-
-        System.out.println("Enter Second line Co-ordinates");
-        System.out.println("Enter the first (a1,b1) point");
-        int a1 = getInputFromUser("a1");
-        int b1 = getInputFromUser("b1");
-        System.out.println("Enter the second (a2, b2) point");
-        int a2 = getInputFromUser("a2");
-        int b2 = getInputFromUser("b2");
-        double a = Math.pow((a2 - a1), 2);
-        double b = Math.pow((b2 - b1), 2);
-        Double length1 = (Math.sqrt(a + b));
-        System.out.println("Length of the line : "+length1);
-        int result = length.compareTo(length1);
+        System.out.println("Welcome to Line Comparison Computation");
+        Double length1=getInput();
+        Double length2=getInput();
+       int result = length1.compareTo(length2);
         System.out.println(result);
 
         if (result == 0) {
-            System.out.println("length = length1");
+            System.out.println("length1 = length2");
         } else if (result > 0) {
-            System.out.println("length > length1");
+            System.out.println("length1 > length2");
         } else if (result < 0) {
-            System.out.println("length < length1");
+            System.out.println("length1 < length2");
         }
     }
-    public static int getInputFromUser(String number) {
-        System.out.println("Enter the " + number + " line co-ordinates");
+    public static double calculatedLength() {
+        double x=Math.pow((x2-x1), 2);
+        double y=Math.pow((y2-y1), 2);
+        double length = Math.sqrt(x+y);
+        System.out.println(length);
+        return length;
+    }
+    public static double getInput() {
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        System.out.println("Enter Line Coordinate for first point");
+        x1=sc.nextInt();
+        y1=sc.nextInt();
+        System.out.println("Enter Line Coordinate for Second point");
+        x2=sc.nextInt();
+        y2=sc.nextInt();
+        calculatedLength();
+        return calculatedLength();
 
     }
 }
